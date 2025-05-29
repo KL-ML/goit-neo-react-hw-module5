7f6769abcf9e7cb8c4dda7aa909c9e4de6e490e1
KL-ML <knataly0513@gmail.com>
2025-05-29 16:50:47 +0300

    Update MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,18 +1,31 @@
-import { Link, useLocation } from "react-router-dom";
+import Container from '../Container/Container';
+import { Link, useLocation } from 'react-router-dom';
+import { RiMovieLine } from 'react-icons/ri';
+import css from './MovieList.module.css';
+import PropTypes from 'prop-types';
 
 export default function MovieList({ movies }) {
   const location = useLocation();
   return (
-    <>
-      <ul>
-        {movies?.map(movie => (
-          <li key={movie.id}>
-            <Link to={`/movies/${movie.id}`} state={location}>
-              {movie.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </>
+    <Container variant="outerContainer">
+      <Container variant="innerContainer">
+        <ul className={css.moviesList}>
+          {movies?.map(movie => (
+            <li className={css.movieLink} key={movie.id}>
+              <div className={css.moviesIcon}>
+                <RiMovieLine size={18} />
+              </div>
+              <Link to={`/movies/${movie.id}`} state={location}>
+                {movie.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Container>
+    </Container>
   );
 }
+
+MovieList.PropTypes = {
+  movies: PropTypes.array,
+};
