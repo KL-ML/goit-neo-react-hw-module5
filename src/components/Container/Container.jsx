b5ddae1722d94577a386e0e16e061fa31614691c
KL-ML <knataly0513@gmail.com>
2025-05-29 16:50:15 +0300

    Update Container.jsx

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -3,12 +3,7 @@ import clsx from 'clsx';
 import css from './Container.module.css';
 
 export default function Container({ children, variant }) {
-  return (
-    <div className={clsx(css.container, css[variant])}>
-      {children}
-      {/* <div className={clsx(css.cardContainer, css[variant])}>{children}</div> */}
-    </div>
-  );
+  return <div className={clsx(css.container, css[variant])}>{children}</div>;
 }
 
 Container.PropTypes = {
